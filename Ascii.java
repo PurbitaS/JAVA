a84371930b9d9444c7167832253033d554144c8a
PurbitaS <84772919+PurbitaS@users.noreply.github.com>
2021-06-11 20:11:29 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,12 @@
+public class Ascii{
+	public static void main(String[] args)
+	{
+		char c1= 'p';
+		char c2= 'u';
+		char c3= 'r';
+		int ascii1= c1, ascii2= c2, ascii3= c3;
+		System.out.println("The ASCII value for the first character 'p' is: " +ascii1);
+		System.out.println("The ASCII value for the second character 'u' is: " +ascii2);
+		System.out.println("The ASCII value for the third character 'r' is: " +ascii3);
+	}
+}
---
+++ Bases.java
@@ -0,0 +1,11 @@
+class Bases{
+	public static void main(String[] args){
+		System.out.println("\t \t \tDNA  Bases\t \t \t");
+		System.out.println(" ");
+		System.out.println("\t BASE \t DESCRIPTION \t COMPLEMENT BASE");
+		System.out.println("\t A \t Adenine \t \t T");
+		System.out.println("\t T \t Thymine \t \t A");
+		System.out.println("\t G \t Guanine \t \t c");
+		System.out.println("\t C \t Cytosine \t \t G");
+	}
+}
