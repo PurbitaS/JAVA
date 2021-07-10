248aa1641d9c230ad8b426c316369c0c0b54f3d9
PurbitaS <84772919+PurbitaS@users.noreply.github.com>
2021-07-10 23:22:00 +0530

    Add files via upload

---
+++ AminoAcids.java
@@ -0,0 +1,30 @@
+class AminoAcids
+{
+	public static void main(String[] args)
+	{
+		double A= 89;
+		double G= 75;
+		double S= 105;
+		double M= 149.2;
+		double C= 121.2;
+		double D= 133.1;
+		double F= 165.2;
+		double Q= 146.1;
+		double N= 132.1;
+		double K= 146.2;
+		System.out.println(" ");
+		System.out.println(" Amino acids and their molecular weights");
+		System.out.println(" ");
+		System.out.println("    Name= Alanine,    MW= " +A);
+		System.out.println("    Name= GLycine,    MW= " +G);
+		System.out.println("    Name= Aspartic Acid, MW= " +D);
+		System.out.println("    Name= Cysteine,   MW= " +C);
+		System.out.println("    Name= Methionine, MW= " +M);
+		System.out.println("    Name= Serine,     MW= " +S);
+		System.out.println("    Name= Phenylalanine, MW= " +F);
+		System.out.println("    Name= Lysine,     MW= " +K);
+		System.out.println("    Name= Asparagine, MW= " +N);
+		System.out.println("    Name= Glutamine,  MW= " +Q);
+	}
+}
+		
