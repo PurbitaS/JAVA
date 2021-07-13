b9e709f37680889f7d41df692dff9ba403c5e2e5
PurbitaS <84772919+PurbitaS@users.noreply.github.com>
2021-07-14 01:07:18 +0530

    Update Checklength.java

--- Checklength.java
+++ Checklength.java
@@ -4,9 +4,26 @@ class Checklength	/*get dna seq from user & find its length; check if they're sa
 	public static void main(String[] args)
 	{
 		Scanner scan = new Scanner(System.in);
-		String DNA= " ";
+		String DNAa= " ";
+		String DNAb= " ";
+		System.out.println(" ");
 		System.out.println("Enter DNA Sequence: ");
-		DNA= scan.nextLine();
-		System.out.println("The length of the DNA sequence is: " +DNA.length());
+		DNAa= scan.nextLine();
+		System.out.println("The length of the DNA sequence is: " +DNAa.length());
+		
+		System.out.println(" ");
+		
+		System.out.println("Enter DNA Sequence: ");
+		DNAb= scan.nextLine();
+		System.out.println("The length of the DNA sequence is: " +DNAb.length());
+		System.out.println(" ");
+		if(DNAa.equals(DNAb))
+		{
+			System.out.println(DNAa.equals(DNAb)+ "\nThe two sequences are same");
+		}
+		else{
+			System.out.println(DNAa.equals(DNAb)+ "\nThe two sequences are not same" );
+		}
+		
 	}
 }
